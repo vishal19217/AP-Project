05d3398197c220f99b88555b64343294015b1561
raj19084 <58364820+raj19084@users.noreply.github.com>
2020-12-06 00:49:29 +0530

    Update Obstacle.java

--- src/sample/Obstacle.java
+++ src/sample/Obstacle.java
@@ -4,6 +4,6 @@ import javafx.animation.Timeline;
 import javafx.scene.shape.Circle;
 
 public abstract class  Obstacle {
-    public abstract void addObstacle(Timeline t1);
+    public abstract void addObstacle(AnchorPane ap,Timeline t1);
     public abstract int checkCollision(Circle ball);
 }
